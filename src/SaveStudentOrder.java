0b83001513b81d0b5bb2b67977225ec5d83e6133
styazhkoV <alexsandr.styazhkov@gmail.com>
2021-06-01 23:23:28 +0600

    Type Data

--- src/SaveStudentOrder.java
+++ src/SaveStudentOrder.java
@@ -1,18 +1,20 @@
 public class SaveStudentOrder {
     public static void main(String[] args) {
+        //Получаю данные на вход
        StudentOrder so;
        so = new StudentOrder();
-       so = hFirstName = "Alexey";
-       so = hLastName = "Petrov";
-       so = wLastName = "Galina";
-       so = wLastName = "Petrova";
+       so.hFirstName = "Alexey";
+       so.hLastName = "Petrov";
+       so.wLastName = "Galina";
+       so.wLastName = "Petrova";
 
        long ans = saveStudentOrder(so);
         System.out.println(ans);
     }
+    //Принимаю данные и обрабатываю их
     static long saveStudentOrder(StudentOrder studentOrder){
         long answer = 199;
-        System.out.println("saveStudentOrder");
+        System.out.println("saveStudentOrder" + studentOrder.hFirstName);
 
         return answer;
     }
