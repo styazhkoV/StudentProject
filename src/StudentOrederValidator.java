f49104209341174bf50c2526320c126620fe76ce
styazhkoV <alexsandr.styazhkov@gmail.com>
2021-06-02 23:53:13 +0600

    StudentOrederValidator while

--- src/StudentOrederValidator.java
+++ src/StudentOrederValidator.java
@@ -1,24 +1,42 @@
 public class StudentOrederValidator {
     public static void main(String[] args) {
-        checkAll();
+       checkAll();
     }
     static void checkAll(){
-        StudentOrder so = readStudentOrder();
         //Чтение студенческой заявки приём данных
-        AnwerSityRegister cityAnswer = checkCityRegister(so);
-        AnswerChildren childrenAnswer = checkChildren(so);
-        AnswerStudent studentAnswer = checkStudent(so);
-        AnswerWedding wedAnswer = checkWedding(so);
-        sendMail(so);
-    }
+        StudentOrder so = readStudentOrder();
+        //Проверка наличия студенческой заявки
+        while (true){
+            System.out.println("start");
+        if (so == null) {
+        //Если нет заявки - останавливаем выполнение
+            break;
+        }
+                System.out.println("finish");
+            AnwerSityRegister cityAnswer = checkCityRegister(so);
+            if (!cityAnswer.succses){
+                //Прекратить обработку и вернуться к следующей заявке
+                continue;
+            }
+            AnswerChildren childrenAnswer = checkChildren(so);
+            AnswerStudent studentAnswer = checkStudent(so);
+            AnswerWedding wedAnswer = checkWedding(so);
+            //Отправка письма
+            sendMail(so);
+            }
+        System.out.println("finish 2");
+        }
+    //Чтение студенческой заявки передача данных
     static StudentOrder readStudentOrder(){
         StudentOrder so = new StudentOrder();
         return so;
-        //Чтение студенческой заявки передача данных
+
     }
     static AnwerSityRegister checkCityRegister(StudentOrder so){
         System.out.println("SityRegister is runing");
-        return new AnwerSityRegister();
+        AnwerSityRegister anwerSityRegister = new AnwerSityRegister();
+        anwerSityRegister.succses = false;
+        return anwerSityRegister;
     }
     static AnswerWedding checkWedding(StudentOrder so){
         System.out.println("Wedding is runing");
@@ -34,6 +52,7 @@ public class StudentOrederValidator {
         return new AnswerStudent();
     }
     static void  sendMail(StudentOrder so){
+        System.out.println("mail send");
 
     }
 }
